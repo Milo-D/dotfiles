6ff5faf43f362880a9832bb2328ad03aee67b921
David Milosevic <milod2048@gmail.com>
2022-12-13 00:32:28 +0100

    Adjusted qtile window movement

--- config/qtile/config.py
+++ config/qtile/config.py
@@ -46,16 +46,16 @@ keys = [
     Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
     # Move windows between left/right columns or move up/down in current stack.
     # Moving out of range in Columns layout will create new column.
-    Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
-    Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
-    Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
-    Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
+    Key([mod, "control"], "Left", lazy.layout.shuffle_left(), desc="Move window to the left"),
+    Key([mod, "control"], "Right", lazy.layout.shuffle_right(), desc="Move window to the right"),
+    Key([mod, "control"], "Down", lazy.layout.shuffle_down(), desc="Move window down"),
+    Key([mod, "control"], "Up", lazy.layout.shuffle_up(), desc="Move window up"),
     # Grow windows. If current window is on the edge of screen and direction
     # will be to screen edge - window would shrink.
-    Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
-    Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
-    Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
-    Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
+    Key([mod, "shift"], "Left", lazy.layout.grow_left(), desc="Grow window to the left"),
+    Key([mod, "shift"], "Right", lazy.layout.grow_right(), desc="Grow window to the right"),
+    Key([mod, "shift"], "Down", lazy.layout.grow_down(), desc="Grow window down"),
+    Key([mod, "shift"], "Up", lazy.layout.grow_up(), desc="Grow window up"),
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
     # Toggle between split and unsplit sides of stack.
     # Split = all windows displayed
